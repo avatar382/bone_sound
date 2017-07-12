4a4a011254472cf414fc072f2a9655d1bbf1dfc4
Manny Rodriguez <manny@7compass.com>
2017-07-12 17:12:07 -0400

    tests for basic session management

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,4 +6,23 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should login" do
+    post login_url
+
+    assert controller.session[:user_id] == 1
+    assert_redirected_to accounts_url
+  end
+
+  test "should logout" do
+    delete logout_url
+
+    assert controller.session[:user_id].nil?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect if not logged in" do
+    get accounts_url
+    
+    assert_redirected_to login_url
+  end
 end
