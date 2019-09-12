f18160fa58781bb5d1d99ec2eb5006fa554bbe93
Manny Rodriguez <manny@theavatar.org>
2019-09-12 16:45:12 -0600

    fix for membership tax charges not sticking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190806210312) do
+ActiveRecord::Schema.define(version: 20190912222257) do
 
   create_table "accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "email"
